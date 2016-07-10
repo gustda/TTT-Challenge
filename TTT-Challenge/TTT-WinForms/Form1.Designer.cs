59c2b9ea84867175ed587d7adb758bbfaef5d92c
gustda <d.gustorff@gmail.com>
2016-07-10 20:34:24 +0200

    Spielfeld wird gezeichnet Req. 7

--- TTT-Challenge/TTT-WinForms/Form1.Designer.cs
+++ TTT-Challenge/TTT-WinForms/Form1.Designer.cs
@@ -28,20 +28,422 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.lblA0 = new System.Windows.Forms.Label();
+            this.lblB0 = new System.Windows.Forms.Label();
+            this.lblC0 = new System.Windows.Forms.Label();
+            this.lblC1 = new System.Windows.Forms.Label();
+            this.lblB1 = new System.Windows.Forms.Label();
+            this.lblA1 = new System.Windows.Forms.Label();
+            this.lblC2 = new System.Windows.Forms.Label();
+            this.lblB2 = new System.Windows.Forms.Label();
+            this.lblA2 = new System.Windows.Forms.Label();
+            this.btnA0 = new System.Windows.Forms.Button();
+            this.btnB0 = new System.Windows.Forms.Button();
+            this.btnC0 = new System.Windows.Forms.Button();
+            this.btnC1 = new System.Windows.Forms.Button();
+            this.btnB1 = new System.Windows.Forms.Button();
+            this.btnA1 = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            this.btnB2 = new System.Windows.Forms.Button();
+            this.btnA2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1, 75);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(76, 49);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1, 75);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(17, 66);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(81, 1);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(17, 85);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(81, 1);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox5.Location = new System.Drawing.Point(17, 123);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(81, 1);
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox6.Location = new System.Drawing.Point(34, 48);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(1, 75);
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox7.Location = new System.Drawing.Point(97, 49);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(1, 75);
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox8.Location = new System.Drawing.Point(17, 104);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(81, 1);
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            // 
+            // lblA0
+            // 
+            this.lblA0.AutoSize = true;
+            this.lblA0.Location = new System.Drawing.Point(38, 69);
+            this.lblA0.Name = "lblA0";
+            this.lblA0.Size = new System.Drawing.Size(14, 13);
+            this.lblA0.TabIndex = 14;
+            this.lblA0.Text = "X";
+            // 
+            // lblB0
+            // 
+            this.lblB0.AutoSize = true;
+            this.lblB0.Location = new System.Drawing.Point(59, 69);
+            this.lblB0.Name = "lblB0";
+            this.lblB0.Size = new System.Drawing.Size(14, 13);
+            this.lblB0.TabIndex = 15;
+            this.lblB0.Text = "X";
+            // 
+            // lblC0
+            // 
+            this.lblC0.AutoSize = true;
+            this.lblC0.Location = new System.Drawing.Point(80, 69);
+            this.lblC0.Name = "lblC0";
+            this.lblC0.Size = new System.Drawing.Size(14, 13);
+            this.lblC0.TabIndex = 16;
+            this.lblC0.Text = "X";
+            // 
+            // lblC1
+            // 
+            this.lblC1.AutoSize = true;
+            this.lblC1.Location = new System.Drawing.Point(80, 90);
+            this.lblC1.Name = "lblC1";
+            this.lblC1.Size = new System.Drawing.Size(14, 13);
+            this.lblC1.TabIndex = 19;
+            this.lblC1.Text = "X";
+            // 
+            // lblB1
+            // 
+            this.lblB1.AutoSize = true;
+            this.lblB1.Location = new System.Drawing.Point(59, 90);
+            this.lblB1.Name = "lblB1";
+            this.lblB1.Size = new System.Drawing.Size(14, 13);
+            this.lblB1.TabIndex = 18;
+            this.lblB1.Text = "X";
+            // 
+            // lblA1
+            // 
+            this.lblA1.AutoSize = true;
+            this.lblA1.Location = new System.Drawing.Point(38, 90);
+            this.lblA1.Name = "lblA1";
+            this.lblA1.Size = new System.Drawing.Size(14, 13);
+            this.lblA1.TabIndex = 17;
+            this.lblA1.Text = "X";
+            // 
+            // lblC2
+            // 
+            this.lblC2.AutoSize = true;
+            this.lblC2.Location = new System.Drawing.Point(80, 108);
+            this.lblC2.Name = "lblC2";
+            this.lblC2.Size = new System.Drawing.Size(14, 13);
+            this.lblC2.TabIndex = 22;
+            this.lblC2.Text = "X";
+            // 
+            // lblB2
+            // 
+            this.lblB2.AutoSize = true;
+            this.lblB2.Location = new System.Drawing.Point(59, 108);
+            this.lblB2.Name = "lblB2";
+            this.lblB2.Size = new System.Drawing.Size(14, 13);
+            this.lblB2.TabIndex = 21;
+            this.lblB2.Text = "X";
+            // 
+            // lblA2
+            // 
+            this.lblA2.AutoSize = true;
+            this.lblA2.Location = new System.Drawing.Point(38, 108);
+            this.lblA2.Name = "lblA2";
+            this.lblA2.Size = new System.Drawing.Size(14, 13);
+            this.lblA2.TabIndex = 20;
+            this.lblA2.Text = "X";
+            // 
+            // btnA0
+            // 
+            this.btnA0.Location = new System.Drawing.Point(35, 67);
+            this.btnA0.Name = "btnA0";
+            this.btnA0.Size = new System.Drawing.Size(20, 18);
+            this.btnA0.TabIndex = 23;
+            this.btnA0.UseVisualStyleBackColor = true;
+            this.btnA0.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnB0
+            // 
+            this.btnB0.Location = new System.Drawing.Point(56, 67);
+            this.btnB0.Name = "btnB0";
+            this.btnB0.Size = new System.Drawing.Size(20, 18);
+            this.btnB0.TabIndex = 24;
+            this.btnB0.UseVisualStyleBackColor = true;
+            this.btnB0.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnC0
+            // 
+            this.btnC0.Location = new System.Drawing.Point(77, 67);
+            this.btnC0.Name = "btnC0";
+            this.btnC0.Size = new System.Drawing.Size(20, 18);
+            this.btnC0.TabIndex = 25;
+            this.btnC0.UseVisualStyleBackColor = true;
+            this.btnC0.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnC1
+            // 
+            this.btnC1.Location = new System.Drawing.Point(77, 86);
+            this.btnC1.Name = "btnC1";
+            this.btnC1.Size = new System.Drawing.Size(20, 18);
+            this.btnC1.TabIndex = 28;
+            this.btnC1.UseVisualStyleBackColor = true;
+            this.btnC1.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnB1
+            // 
+            this.btnB1.Location = new System.Drawing.Point(56, 86);
+            this.btnB1.Name = "btnB1";
+            this.btnB1.Size = new System.Drawing.Size(20, 18);
+            this.btnB1.TabIndex = 27;
+            this.btnB1.UseVisualStyleBackColor = true;
+            this.btnB1.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnA1
+            // 
+            this.btnA1.Location = new System.Drawing.Point(35, 86);
+            this.btnA1.Name = "btnA1";
+            this.btnA1.Size = new System.Drawing.Size(20, 18);
+            this.btnA1.TabIndex = 26;
+            this.btnA1.UseVisualStyleBackColor = true;
+            this.btnA1.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(77, 105);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(20, 18);
+            this.btnC2.TabIndex = 31;
+            this.btnC2.UseVisualStyleBackColor = true;
+            this.btnC2.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnB2
+            // 
+            this.btnB2.Location = new System.Drawing.Point(56, 105);
+            this.btnB2.Name = "btnB2";
+            this.btnB2.Size = new System.Drawing.Size(20, 18);
+            this.btnB2.TabIndex = 30;
+            this.btnB2.UseVisualStyleBackColor = true;
+            this.btnB2.Click += new System.EventHandler(this.GameBtnClick_Click);
+            // 
+            // btnA2
+            // 
+            this.btnA2.Location = new System.Drawing.Point(35, 105);
+            this.btnA2.Name = "btnA2";
+            this.btnA2.Size = new System.Drawing.Size(20, 18);
+            this.btnA2.TabIndex = 29;
+            this.btnA2.UseVisualStyleBackColor = true;
+            this.btnA2.Click += new System.EventHandler(this.GameBtnClick_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(432, 378);
+            this.Controls.Add(this.btnC2);
+            this.Controls.Add(this.btnB2);
+            this.Controls.Add(this.btnA2);
+            this.Controls.Add(this.btnC1);
+            this.Controls.Add(this.btnB1);
+            this.Controls.Add(this.btnA1);
+            this.Controls.Add(this.btnC0);
+            this.Controls.Add(this.btnB0);
+            this.Controls.Add(this.btnA0);
+            this.Controls.Add(this.lblC2);
+            this.Controls.Add(this.lblB2);
+            this.Controls.Add(this.lblA2);
+            this.Controls.Add(this.lblC1);
+            this.Controls.Add(this.lblB1);
+            this.Controls.Add(this.lblA1);
+            this.Controls.Add(this.lblC0);
+            this.Controls.Add(this.lblB0);
+            this.Controls.Add(this.lblA0);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "TTT-WinForms";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label lblA0;
+        private System.Windows.Forms.Label lblB0;
+        private System.Windows.Forms.Label lblC0;
+        private System.Windows.Forms.Label lblC1;
+        private System.Windows.Forms.Label lblB1;
+        private System.Windows.Forms.Label lblA1;
+        private System.Windows.Forms.Label lblC2;
+        private System.Windows.Forms.Label lblB2;
+        private System.Windows.Forms.Label lblA2;
+        private System.Windows.Forms.Button btnA0;
+        private System.Windows.Forms.Button btnB0;
+        private System.Windows.Forms.Button btnC0;
+        private System.Windows.Forms.Button btnC1;
+        private System.Windows.Forms.Button btnB1;
+        private System.Windows.Forms.Button btnA1;
+        private System.Windows.Forms.Button btnC2;
+        private System.Windows.Forms.Button btnB2;
+        private System.Windows.Forms.Button btnA2;
     }
 }
 
--- TTT-Challenge/TTT-WinForms/Form1.cs
+++ TTT-Challenge/TTT-WinForms/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameLib;
+using GameLib.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,79 @@ namespace TTT_WinForms
 {
     public partial class Form1 : Form
     {
+        Game ActGame = new Game();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GameBtnClick_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            var name = btn.Name;
+            // get coordinate string from button name
+            var cords= name.Remove(0, 3);
+
+            // TODO do someting with the cords
+            ActGame.Gameboard['a'][0] = GameStoneState.PlayerOne;
+            ActGame.Gameboard['a'][1] = GameStoneState.PlayerTwo;
+            // reprint field
+            ShowField();
+        }
+
+        private void ShowField()
+        {             
+                 SetOneField("A0", ActGame.Gameboard['a'][0]);
+                 SetOneField("A1", ActGame.Gameboard['a'][1]);
+                 SetOneField("A2", ActGame.Gameboard['a'][2]);
+
+                 SetOneField("B0", ActGame.Gameboard['b'][0]);
+                 SetOneField("B1", ActGame.Gameboard['b'][1]);
+                 SetOneField("B2", ActGame.Gameboard['b'][2]);
+
+                 SetOneField("C0", ActGame.Gameboard['c'][0]);
+                 SetOneField("C1", ActGame.Gameboard['c'][1]);
+                 SetOneField("C2", ActGame.Gameboard['c'][2]);
+                    }
+
+        private void SetOneField(string cords, GameStoneState value)
+        {
+            foreach(Control c in this.Controls)
+            {
+                if(c.Name.Contains(cords))
+                {
+                    if(c.Name=="lbl"+cords)
+                    {
+                        var lbl = (Label)c;
+                        switch(value)
+                        {
+                            case GameStoneState.Free:
+                                lbl.Text = "";
+                                break;
+                            case GameStoneState.PlayerOne:
+                                lbl.Text = "X";
+                                break;
+                            case GameStoneState.PlayerTwo:
+                                lbl.Text = "O";
+                                break;
+                        }
+                    }
+                    if (c.Name == "btn" + cords)
+                    {
+                        var btn = (Button)c;
+                        switch (value)
+                        {
+                            case GameStoneState.Free:
+                                btn.Visible=true;
+                                break;
+                            case GameStoneState.PlayerOne:                               
+                            case GameStoneState.PlayerTwo:
+                                btn.Visible = false;
+                                break;
+                        }
+                    }    
+                }
+            }
         }
     }
 }
